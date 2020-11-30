9efafd764891a26e49ff16715b9a01c10c1e4c01
renjeka <renjeka@gmail.com>
2020-11-30 18:48:37 +0200

    добавлены Ñ2 таблицы с фиксированной первой колонкофй

--- src/app/components/table-simple/table-simple.component.ts
+++ src/app/components/table-simple/table-simple.component.ts
@@ -12,6 +12,7 @@ export class TableSimpleComponent implements OnInit {
 
   cars: Car[];
   cols: any[];
+  frCols: any[];
 
   constructor(private carService: CarService) { }
 
@@ -21,13 +22,20 @@ export class TableSimpleComponent implements OnInit {
       this.cars = cars
     });
     this.cols = [
-      { field: 'vin', header: 'Vin', type: 'text' },
       { field: 'year', header: 'Year', type: 'text' },
       { field: 'brand', header: 'Brand', type: 'text' },
       { field: 'color', header: 'Color', type: 'text' },
       { field: 'a', header: 'A', type: 'checkbox' },
       { field: 'b', header: 'B', type: 'checkbox' },
+      { field: 'c', header: 'C', type: 'checkbox' },
+      { field: 'd', header: 'D', type: 'checkbox' },
+      { field: 'e', header: 'E', type: 'text' },
     ];
+
+    this.frCols = [
+      {field: 'vin', header: 'Vin', type: 'text' }
+    ];
+
   }
 
   // logFunction($event: any) {
